a394f904e2a4d98a1e6cfe0ce9368d8716cf452c
yudaheed <40046439+yudaheed@users.noreply.github.com>
2018-08-16 16:27:09 +0900

    0816

--- Broken Lich of Dungeon/Assets/Scripts/Manager/User_Manager.cs
+++ Broken Lich of Dungeon/Assets/Scripts/Manager/User_Manager.cs
@@ -11,28 +11,31 @@ using UnityEngine.UI;
 public class User_Manager : MonoBehaviour
 {
     #region 멤버 변수
-    //attack = power*2 + weapon
-    //hp = health *20
+    //attack = power*2 + weapon_Damage
+    //hp = health *20 + weapon_Health
 
     public static User_Manager instance;
 
-    public static float gold;//유저의 소지 골드
+    public static float gold = 0;//유저의 소지 골드
 
     //유저의 정보
-    public static float LV;// 유저 레벨
-    //외부 요인 따라 변하는 유저의 정보
-    public static float attack;// 공격력
-    public static float hp;// 체력
-    public static float max_hp;// 최대 체력
-
+    public static float LV = 1;// 유저 레벨
+    
     //스텟
-    public static float power;// 힘
-    public static float health;// 헬스
+    public static float power = 5;// 힘
+    public static float health = 5;// 헬스
+
+    //외부 요인 따라 변하는 유저의 정보
+    public static float attack = power * 2 + weapon_Damage;// 공격력
+    public static float max_hp = health * 20 + weapon_Health;// 최대 체력
+    public static float hp = max_hp;// 체력
 
     //유저의 장비
-    public static string right_weapon_slot;// 장비 슬롯
+    public static string right_weapon_slot = "스틸 대거";// 오른손 장비 슬롯
+    public static string left_weapon_slot = "버클러";// 왼손 장비 슬롯
     //장착한 장비의 데미지
     public static float weapon_Damage;
+    public static float weapon_Health;
 
     //유저가 생존했는지 확인하는 변수
     public static bool alive = true;
@@ -44,6 +47,7 @@ public class User_Manager : MonoBehaviour
     public Text text_power;
     public Text text_health;
     public Text text_right_weapon;
+    public Text text_left_weapon;
     public Text text_attackPoint;
     #endregion
 
@@ -64,18 +68,6 @@ public class User_Manager : MonoBehaviour
     #region 시작 함수
     void Start()
     {
-        gold = 0;//돈을 0으로 초기화
-        LV = 1; //LV을 1로 초기화
-        //스텟 초기화
-        power = 5;
-        health = 5;
-        right_weapon_slot = "스틸 대거";
-        //스텟공식
-        attack = power * 2 + weapon_Damage;
-        max_hp = health * 20;
-
-        hp = max_hp;//hp를 최대치로 초기화
-
         alive = true;
 
         tempColor = new Color();
